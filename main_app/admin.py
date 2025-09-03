ddbe400bbdec4995a079d817a85f6d47f3bffe12
HassanAbbas107 <qiqbx223@gmail.com>
2025-09-03 10:59:18 +0300

    some views and auth

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Call,Message
+
+
+
 
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Call)
+admin.site.register(Message)
--- main_app/views.py
+++ main_app/views.py
@@ -4,11 +4,48 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.views import View
 from .models import Call, Message
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+import requests
+from .forms import CallForm,MessageForm
 
 # Create your views here.
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = 'registration/sign-up.html'
+
+
+#Calls Views
+class CallCreateView(CreateView):
+    model=Call
+    form_class=CallForm
+    template_name = "Calls/call_form.html"
+    
+    def get_success_url(self):
+        return reverse("call_detail", kwargs={"pk": self.object.pk})
+    
+    
 class CallsListView(LoginRequiredMixin,ListView):
     model=Call
-    LoginRequiredMixin = "Calls/Call_list.html"
+    LoginRequiredMixin = "Calls/call_list.html"
     context_object_name = "Call"    
     def get_queryset(self):
         return super().get_queryset()
+
+class CallDetailView(DetailView):
+    model = Call
+    template_name = "Calls/call_detail.html"
+    context_object_name = "Call"
+
+class CallDelete(DeleteView):
+    model = Call 
+    template_name= "Calls/call_delete.html"    
+    success_url = reverse_lazy("call_list")
+    
+    
+#Message Views
+
+
