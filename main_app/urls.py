0fddfb333495e5a42c02469add89785eec97c6da
HassanAbbas107 <qiqbx223@gmail.com>
2025-09-03 10:02:08 +0300

    calls list views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
-
+path("Calls",views.CallsListView.as_view(),name="call_list")
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import View
+from .models import Call, Message
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
+class CallsListView(LoginRequiredMixin,ListView):
+    model=Call
+    LoginRequiredMixin = "Calls/Call_list.html"
+    context_object_name = "Call"    
+    def get_queryset(self):
+        return super().get_queryset()
