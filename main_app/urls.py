301e71e9fb68fb9d18737ba8a70c82ae00d5c650
HassanAbbas107 <qiqbx223@gmail.com>
2025-09-08 09:56:04 +0300

    auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,15 +6,15 @@ urlpatterns = [
 path("calls/",views.CallsListView.as_view(),name="call_list"),
 path("calls/new/",views.CallCreateView.as_view(),name="call_create"),
 path("calls/<int:pk>/delete/",views.CallDelete.as_view(),name='call_delete'),
-path("Calls/<int:call_id>/",views.CallDetailView.as_view(),name="call_detail"),
+path("calls/<int:call_id>/",views.CallDetailView.as_view(),name="call_detail"),
 
 
 # Messages url  
-path("Messages/new/",views.MessageCreateView.as_view(),name="message_create"),
-path("Messages/",views.MessageListView.as_view(),name="messages_list"),
-path("Messages/<int:message_id>/",views.MessageDetailView.as_view(),name="message_details"),
-path("Messages/<int:message_id>/update/",views.MessageUpdateView.as_view(),name="message_create"),
-path("Messages/<int:pk>/delete/",views.MessageDeleteView.as_view(),name="message_delete"),
+path("messages/new/",views.MessageCreateView.as_view(),name="message_create"),
+path("messages/",views.MessageListView.as_view(),name="messages_list"),
+path("messages/<int:message_id>/",views.MessageDetailView.as_view(),name="message_details"),
+path("messages/<int:message_id>/update/",views.MessageUpdateView.as_view(),name="message_create"),
+path("messages/<int:pk>/delete/",views.MessageDeleteView.as_view(),name="message_delete"),
 path("auth/signup",views.SignUpView.as_view(), name="signup"),
 
 ]
